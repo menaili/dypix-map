ff037922bab833216fb7d9ec2abc4318c90aaec1
Menaili Haroun Errachide <85519182+menaili@users.noreply.github.com>
2023-05-07 11:54:11 +0100

    add two tip of markers

--- index.js
+++ index.js
@@ -8,21 +8,34 @@ function initMap() {
       zoom: 8,
       center: { lat: 36.291715, lng: 2.8331188 },
     });  
-    setMarkers(map);
+    setMarkerspart(map);
+    setMarkerspro(map);
+
   }  
-  let beaches = [];
+  // let beaches = [];
+  // for (let i = 0; i < data.length; i++) {
+  //   beaches.push( [data[i].ville, parseFloat(data[i].lat), parseFloat(data[i].lng), data[i].client] );    
+  // }
+
+
+  let imagePro = [];
+  let imagePart = [];
+
   for (let i = 0; i < data.length; i++) {
-    beaches.push( [data[i].ville, parseFloat(data[i].lat), parseFloat(data[i].lng), data[i].client] );    
-  }
+    if (data[i].client == "Particuliers" && data[i].status == "1") {
+      imagePart.push( [data[i].client, parseFloat(data[i].lat), parseFloat(data[i].lng), data[i].willaya] ); 
+    }
+    else if (data[i].client == "Professionnels" && data[i].status == "1") {
+      imagePro.push( [data[i].client, parseFloat(data[i].lat), parseFloat(data[i].lng), data[i].willaya] ); 
+    }
+      
+  }   
+  console.log(imagePart);
+  console.log(imagePro);
 
-  const filter = 'nature';
-  const filteredResult = initialState.filter((item) => {
-      return (item.tags.indexOf(filter) >= 0);
-  });
-console.log(filteredData);
+  function setMarkerspart(map) {
 
-  //console.log(beaches);
-  function setMarkers(map) {   
+
     const image = {
       url: "marker.png",
       size: new google.maps.Size(34, 51),
@@ -33,23 +46,59 @@ console.log(filteredData);
       coords: [1, 1, 1, 51, 34, 51, 34, 1],
       type: "poly",
     };  
-    for (let i = 0; i < beaches.length; i++) {
-      const beach = beaches[i];  
+    for (let i = 0; i < imagePart.length; i++) {
+      const imageP = imagePart[i];  
       Marker = new google.maps.Marker({
-        position: { lat: beach[1], lng: beach[2] },
+        position: { lat: imageP[1], lng: imageP[2] },
         map,
         icon: image,
         shape: shape,
-        title: beach[0],
-        zIndex: beach[3],
+        title: imageP[0],
+        zIndex: imageP[3],
       });
       Marker.addListener('click', function() {
         const map = new google.maps.Map(document.getElementById("map"), {
           zoom: 14,
-          center: { lat: beach[1], lng: beach[2]  },
+          center: { lat: imageP[1], lng: imageP[2]  },
         });
-        setMarkers(map);    
+        setMarkerspart(map);    
       });
     }   
   }
+
+  function setMarkerspro(map) {
+
+
+    const image = {
+      url: "Baies-marker.png",
+      size: new google.maps.Size(34, 51),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 51),
+    };
+    const shape = {
+      coords: [1, 1, 1, 51, 34, 51, 34, 1],
+      type: "poly",
+    };  
+    for (let i = 0; i < imagePro.length; i++) {
+      const imagePr = imagePro[i];  
+      Marker = new google.maps.Marker({
+        position: { lat: imagePr[1], lng: imagePr[2] },
+        map,
+        icon: image,
+        shape: shape,
+        title: imagePr[0],
+        zIndex: imagePr[3],
+      });
+      Marker.addListener('click', function() {
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 14,
+          center: { lat: imagePr[1], lng: imagePr[2]  },
+        });
+        setMarkerspro(map);    
+      });
+    }   
+  }
+
+
+  
   window.initMap = initMap;
